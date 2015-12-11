d658a3fa8dd49ecd42341051ba7f023f97553639
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-12-11 19:51:55 +0100

    Add Activity generator

--- src/test/scala/com/kastrull/tajm/Generators.scala
+++ src/test/scala/com/kastrull/tajm/Generators.scala
@@ -2,6 +2,14 @@ package com.kastrull.tajm
 
 object Generators {
 
+  import org.scalacheck._
+  import Gen._
+  import Arbitrary.arbitrary
+
+  val genActivity = for {
+    path <- arbitrary[Seq[String]]
+  } yield Activity(path)
+
 }
 
 // FIXME Empty object
