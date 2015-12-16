27bc1b2658baccc0b49e1afc11ebbd8e10f73c15
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-12-16 23:23:37 +0100

    Extract Activity to separate file

--- src/main/scala/com/kastrull/tajm/model/Command.scala
+++ src/main/scala/com/kastrull/tajm/model/Command.scala
@@ -57,31 +57,6 @@ case class Time(minutes: Int)
 
 case class TimeRange(from: Time, to: Time)
 
-object Activity {
-  def apply(): Activity = Activity(Nil)
-  def apply(p1: String): Activity = Activity(p1 :: Nil)
-  def apply(p1: String, p2: String): Activity = Activity(p1 :: p2 :: Nil)
-  def apply(p1: String, p2: String, p3: String): Activity = Activity(p1 :: p2 :: p3 :: Nil)
-}
-
-case class Activity(path: Seq[String]) {
-
-  def isParentOf(other: Activity): Boolean = {
-    val depth = path.length
-    def otherIsLonger = other.path.length > depth
-    def equalPrefix = other.path.take(depth) == path
-
-    otherIsLonger && equalPrefix
-  }
-
-  def isChildOf(other: Activity) =
-    other.isParentOf(this)
-
-  override def toString(): String =
-    "Activity(" + path.map("'" + _ + "'").mkString(", ") + ")"
-
-}
-
 case class Day(
   date: LocalDate,
   lines: Seq[LineCommand] = Nil,
