f6a726727a386952ec1426e5879d57b5c28102fe
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-01-09 09:38:13 +0100

    WIP Play around with ConsoleTajm App

--- src/main/scala/com/kastrull/tajm/ConsoleTajm.scala
+++ src/main/scala/com/kastrull/tajm/ConsoleTajm.scala
@@ -1,6 +1,17 @@
 package com.kastrull.tajm
 
+import scala.io.Source
+import java.io.FileReader
+import com.kastrull.tajm.parse._
+
 object ConsoleTajm extends App {
+  val fileName = args(0)
+
+  val dataSource = Source.fromFile(fileName)
+  val parser: Parser = NormalFormParser
+
+  for (line <- dataSource.getLines()) {
+    println(parser.parseLine(line))
+  }
   // TODO create program
-  println("Hello Tajm!")
 }
